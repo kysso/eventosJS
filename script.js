8817f055fd04ba3ee567860f58c5031d535acb29
Marcos Vinicius <eumarquinhoo@gmail.com>
2023-02-03 11:35:15 -0300

    eventos

---
+++ script.js
@@ -0,0 +1,19 @@
+function clickMe() {
+    console.log("Fui clicado")
+}
+
+function FocusMe() {
+    console.log("Fui focado")
+}
+
+function blurMe() {
+    console.log("fui desfocado")
+}
+
+function changeMe() {
+    console.log("Fui alterado")
+}
+
+function overMe() {
+    console.log("Mouse aqui")
+}
